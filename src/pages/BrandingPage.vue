92893a5b6fad8f569c3fc59ce7c64d77ff1a81f4
Léo Guillaume <leo.guillaume@epitech.eu>
2023-01-09 17:08:55 +0100

    logo component

--- src/pages/BrandingPage.vue
+++ src/pages/BrandingPage.vue
@@ -1,16 +1,23 @@
 <template>
-  <div>
-    <h1>Branding</h1>
+  <section class="mx-4">
+    <h1 class="mb-2">Branding</h1>
 
-    <h2>Avatar</h2>
-    <Avatar>N</Avatar>
-    <Avatar></Avatar>
-    <Avatar size="44px" src="/images/women.png"/>
-    <Avatar size="44px" src="/images/women.png" online/>
-  </div>
+    <h2 class="mb-2">Avatar</h2>
+    <div class="d-flex gap-2">
+      <Avatar>N</Avatar>
+      <Avatar></Avatar>
+      <Avatar size="44px" src="/images/women.png"/>
+      <Avatar size="44px" src="/images/women.png" online/>
+    </div>
+
+    <h2 class="mb-2">Logo</h2>
+    <Logo/>
+    <Logo large/>
+  </section>
 </template>
 
 
 <script lang="ts" setup>
 import Avatar from "@/components/Common/Avatar.vue"
+import Logo from "@/components/Common/Logo.vue"
 </script>
