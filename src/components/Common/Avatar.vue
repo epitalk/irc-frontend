7ce394fedc47ddaccc6eca4d2554e6db6b5391f5
Léo Guillaume <leo.guillaume@epitech.eu>
2023-01-10 11:28:51 +0100

    sidebar && sidebar link component

--- src/components/Common/Avatar.vue
+++ src/components/Common/Avatar.vue
@@ -16,8 +16,8 @@ const slotHasContent = ref(slot.default !== undefined)
 
 console.log(slot.default)
 const props = defineProps({
-  size: {type: String, default: '44px'},
-  borderRadius: {type: String, default: '12px'},
+  size: {type: String, default: '40px'},
+  borderRadius: {type: String, default: '8px'},
   src: {type: String, default: null},
   alt: {type: String, default: null},
   online: {type: Boolean, default: false},
--- src/components/Navigations/Sidebar/index.vue
+++ src/components/Navigations/Sidebar/index.vue
@@ -1,13 +1,8 @@
 <template>
-  <nav class="sidebar" :class="{ expand }">
-    <button class="toggle" @click="onExpand">
-      <Icon name="chevron-left" v-if="expand === true"/>
-      <Icon name="chevron-right" v-else/>
-    </button>
-
+  <nav class="sidebar expand">
     <div class="d-flex column between h-full">
-      <div class="p-1 bb-1 w-full">
-        <Logo :large="expand"/>
+      <div class="p-2 bb-1 w-full">
+        <Logo :size="28" large/>
       </div>
       <ul class="d-flex column center-y px-1 py-2 h-full gap-2">
         <side-bar-link
@@ -15,29 +10,22 @@
             v-for="item in items"
             :key="item.to"
             :item="item"
-            :icon="item.icon"
-            :large="expand"
+            large
         />
       </ul>
       <div class="d-flex gap-1 bt-1 p-1 center-y">
-        <Avatar src="assets/images/avatar.png" alt="username"/>
-        <span v-if="expand">username</span>
+        <Avatar src="/images/anonyme-user.jpg" alt="username"/>
+        <span>username</span>
       </div>
     </div>
   </nav>
 </template>
 <script lang="ts" setup>
-import { ref } from 'vue';
-import Icon from "@/components/Common/Icon.vue"
 import Logo from "@/components/Brands/Logo.vue"
-import SideBarLink from "@/components/Navigations/Sidebar/SideBarLink/index.vue"
+import SideBarLink from "@/components/Navigations/Sidebar/SideBarLink.vue"
 import Avatar from "@/components/Common/Avatar.vue"
 import items from "./items.json"
 import { useRoute } from "vue-router";
-
-
-const expand = ref(false)
-const onExpand = () => expand.value = !expand.value
 
 const route = useRoute()
 </script>
