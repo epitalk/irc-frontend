96f623f3255c531332359fc0407fae508c055b8a
Léo Guillaume <leo.guillaume@epitech.eu>
2023-01-17 14:21:10 +0100

    receive sse messages

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,14 +5,15 @@
         <div class="d-flex center-y gap-2">
           <Avatar borderRadius="12px"
                   size="40px"
-                  online>NA</Avatar>
+                  online>NA
+          </Avatar>
           <h6>conversion name</h6>
         </div>
-        <Icon name="more-vertical"/>
+        <Icon name="more-vertical" />
       </header>
-      <Messages :messages="messages"/>
+      <Messages :messages="messages" />
       <div class="bg-grey-500 p-2 d-flex center-x bt-1">
-        <ChatInput @addNewMessage="addNewMessage"/>
+        <ChatInput @addNewMessage="addNewMessage" />
       </div>
     </div>
   </section>
@@ -20,31 +21,36 @@
 
 
 <script lang="ts" setup>
-import Messages from "@/components/Messaging/Messages.vue"
-import Avatar from "@/components/Common/Avatar.vue"
-import Icon from "@/components/Common/Icon.vue"
-import messages from "@/data/messages.json"
-import ChatInput from "@/components/Fields/ChatInput.vue"
+import Messages from "@/components/Messaging/Messages.vue";
+import Avatar from "@/components/Common/Avatar.vue";
+import Icon from "@/components/Common/Icon.vue";
+import ChatInput from "@/components/Fields/ChatInput.vue";
 import { ref, watch } from "vue";
 import { API_URL, MERCURE_URL } from "@/env";
-import axios from "axios"
+import axios from "axios";
+import type { Message as MessageType } from "@/api/message/messages";
 
 /*MERCURE SSE*/
-const messagesSse = ref([] as string[])
-const url = new URL(MERCURE_URL)
-const eventSource = new EventSource(url, { withCredentials: true })
-eventSource.onmessage = (e) => messagesSse.value.push(JSON.parse(e.data).message)
+const messages = ref([] as MessageType[]);
+const url = new URL(MERCURE_URL);
+url.searchParams.append("topic", "/chat");
+const eventSource = new EventSource(url, { withCredentials: true });
+eventSource.onmessage = (e) => messages.value.push(JSON.parse(e.data).message);
 /* Fix firefox warning */
-window.addEventListener("beforeunload", () => eventSource.close())
+window.addEventListener("beforeunload", () => eventSource.close());
 
 /*METHODS*/
 const addNewMessage = (message: string) => {
-  axios.post(API_URL + "/chat/public", { message })
-}
+  axios.post(API_URL + "/chat/public", {
+    message: {
+      content: message
+    }
+  });
+};
 
 /*WATCHERS*/
-watch(() => messagesSse.value, (value) => {
-  console.log(value)
-});
+watch(() => messages.value, (value) => {
+  console.log("bidule", value);
+}, { deep: true });
 
 </script>
