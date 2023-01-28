18d1604862b09d0f4f0c6ea86622f5d39ab65d9b
Léo Guillaume <leo.guillaume@gmail.com>
2023-01-28 17:17:16 +0100

    fix: beug create channel fixed

--- src/api/channel/channel.ts
+++ src/api/channel/channel.ts
@@ -6,8 +6,8 @@ import type { ChannelModel } from "@/api/channel/channel.model";
 export class ChannelApi {
   static channelApiUrl = API_URL + "/api/channel";
 
-  static async createChannel(channelName: string) {
-    await axios.post(this.channelApiUrl, { name: channelName });
+  static async createChannel(channelName: string, author: string) {
+    await axios.post(this.channelApiUrl, { name: channelName, author });
   }
 
   static async addUserChannel(channel: string, username: string) {
--- src/services/SseService.ts
+++ src/services/SseService.ts
@@ -12,12 +12,14 @@ export class SseService {
   private static notyf = new Notyf({ position: { x: "right", y: "top" } });
 
   static createChannel = async (channelName: string) => {
-    if(ChannelService.findChannelByName(channelName) || adminChannels.includes(channelName)){
-      return this.notyf.error(`Le channel ${channelName} existe déjà !`)
+    const userStore = useUserStore();
+    if (ChannelService.findChannelByName(channelName) || adminChannels.includes(channelName)) {
+      return this.notyf.error(`Le channel ${channelName} existe déjà !`);
     }
-    await ChannelApi.createChannel(channelName)
-    await router.push('/channel/' + channelName)
-  }
+    await ChannelApi.createChannel(channelName, userStore.user.username);
+
+    await router.push("/channel/" + channelName);
+  };
 
   static initSseChannels = () => {
     const channelStore = useChannelStore();
@@ -37,21 +39,21 @@ export class SseService {
 
   static connectToTopic(topic: string): EventSource | undefined {
     /*Channel SSE */
-    const channelStore = useChannelStore()
+    const channelStore = useChannelStore();
     const url = new URL(MERCURE_URL);
-    const channel = ChannelService.findChannelByName(topic)
+    const channel = ChannelService.findChannelByName(topic);
 
-    if (channel && topic !== "topics"){
-       const userStore = useUserStore()
+    if (channel && topic !== "topics") {
+      const userStore = useUserStore();
 
-       const userIsInChannel = channel.users.find(u => u.id === userStore.user.id) !== undefined
-       if (!userIsInChannel){
-           ChannelApi.addUserChannel(topic, userStore.user.username).then(() => {
-             channelStore.addUserChannel(topic, userStore.user)
-           }).catch(() => {
-               this.notyf.error("Erreur lors de l'ajout de l'utilisateur au channel")
-           })
-       }
+      const userIsInChannel = channel.users?.find(u => u.id === userStore.user.id) !== undefined;
+      if (!userIsInChannel) {
+        ChannelApi.addUserChannel(topic, userStore.user.username).then(() => {
+          channelStore.addUserChannel(topic, userStore.user);
+        }).catch(() => {
+          this.notyf.error("Erreur lors de l'ajout de l'utilisateur au channel");
+        });
+      }
     }
 
     url.searchParams.append("topic", topic);
@@ -81,16 +83,16 @@ export class SseService {
   }
 
   static async leaveChannel(channelName: string) {
-    const userStore = useUserStore()
-    const channelSTore = useChannelStore()
+    const userStore = useUserStore();
+    const channelSTore = useChannelStore();
     if (!ChannelService.findChannelByName(channelName)) {
-      return this.notyf.error(`Le channel ${channelName} n'existe pas !`)
+      return this.notyf.error(`Le channel ${channelName} n'existe pas !`);
     }
-    await ChannelApi.removeUserChannel(channelName, userStore.user.username)
-    this.eventSource?.close()
+    await ChannelApi.removeUserChannel(channelName, userStore.user.username);
+    this.eventSource?.close();
 
-    if (channelName === channelSTore.currentChannel){
-      await router.push('/general')
+    if (channelName === channelSTore.currentChannel) {
+      await router.push("/general");
     }
   }
 
@@ -99,8 +101,8 @@ export class SseService {
     const userStore = useUserStore();
 
     await axios.post(API_URL + "/api/message/channel/" + channelStore.currentChannel, {
-        username: userStore.user?.username,
-        content: message
+      username: userStore.user?.username,
+      content: message
     });
   }
 }
